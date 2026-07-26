862a4627ae6206d168603edbdfc1e73bad6eb571
Shansong Huang <tech.shansong@hotmail.com>
2026-07-26 13:21:06 +0930

    refactor(repository): add constraint repository

--- backend/app/repositories/__init__.py
+++ backend/app/repositories/__init__.py
@@ -1,8 +1,10 @@
+from .constraint_repository import ConstraintRepository
 from .obstacle_repository import ObstacleRepository
 from .scenario_repository import ScenarioRepository
 from .vehicle_profile_repository import VehicleProfileRepository
 
 __all__ = [
+    "ConstraintRepository",
     "ObstacleRepository",
     "ScenarioRepository",
     "VehicleProfileRepository",
