1a1231f15d0ea22f9cf33df36af2e84868c58eda
Shansong Huang <tech.shansong@hotmail.com>
2026-07-26 13:34:49 +0930

    refactor(repository): add external field repository

--- backend/app/repositories/__init__.py
+++ backend/app/repositories/__init__.py
@@ -1,10 +1,12 @@
 from .constraint_repository import ConstraintRepository
+from .external_field_repository import ExternalFieldRepository
 from .obstacle_repository import ObstacleRepository
 from .scenario_repository import ScenarioRepository
 from .vehicle_profile_repository import VehicleProfileRepository
 
 __all__ = [
     "ConstraintRepository",
+    "ExternalFieldRepository",
     "ObstacleRepository",
     "ScenarioRepository",
     "VehicleProfileRepository",
